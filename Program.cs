6790e0431a1af11b813d07057fc70728e8df2e31
HASBIYALLAH OYEBO <143263920+hasbiyallah01@users.noreply.github.com>
2024-07-03 15:31:12 +0100

    Update Prog

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Net;
-using HNGTask2.Client;
+using HNGTask2.Controllers;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// forward headers configuration for reverse proxy
+// Forward headers configuration for reverse proxy
 builder.Services.Configure<ForwardedHeadersOptions>(options => {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
     options.KnownNetworks.Clear();
@@ -29,6 +28,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
 app.UseSwagger();
 app.UseSwaggerUI();
 app.UseFileServer();
